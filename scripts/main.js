887b1c7f9ab1c27de944c9cad7fea08ec353666a
Siddharth Jha <siddharth99c@gmail.com>
2021-09-28 00:00:35 +0530

    feat: stars and clouds

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,5 @@
 // this import will transpile sass/scss to css
 import "../styles/main.scss";
+import starryNight from "./starryNight";
 
-function TestVite() {
-	setTimeout(() => {
-		if (import.meta.env.DEV === true) {
-			console.log("vite is working!");
-		}
-		if (import.meta.env.PROD === true) {
-			console.log("vite built, and 11ty injected correct script tag");
-		}
-	}, 400);
-}
-
-TestVite();
+// starryNight();
