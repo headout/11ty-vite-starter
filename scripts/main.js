1a3d0185f371f96fe03b26708902987ba6f4b64a
Siddharth Jha <siddharth99c@gmail.com>
2021-10-05 23:50:20 +0530

    ✌️ added intro animation

--- scripts/main.js
+++ scripts/main.js
@@ -8,12 +8,57 @@ import LocomotiveScroll from 'locomotive-scroll'
 
 let scroll;
 
+const introAnim = () => {
+	const intro = anime.timeline({
+		targets: '#header-year > path',
+		strokeDashoffset: [anime.setDashoffset, 0],
+		easing: 'easeInOutSine',
+		duration: 2000,
+	}).add({
+		targets: '#header-wordmark > svg > path',
+		translateY: [50, 0],
+		translateX: [10, 0],
+		opacity: [0, 1],
+		duration: 1200,
+		delay: anime.stagger(-100),
+		easing: 'easeInOutQuad'
+	}, 2000).add({
+		targets: '.header-dates > span > .char',
+		translateY: [0, -10],
+		easing: 'easeInOutSine',
+		duration: 200,
+		opacity: [0, 1],
+		delay: (el, i) => i * 10
+	}, '+=400').add({
+		targets: '.header-subtitle',
+		easing: 'easeInOutQuad',
+		translateY: [10, 0],
+		opacity: [0, 1],
+		duration: 400,
+	}, '+=800').add({
+		targets: '.header-cta-wrapper',
+		easing: 'easeInOutQuad',
+		opacity: [0, 1],
+		translateY: [10, 0],
+		duration: 200
+	}).add({
+		targets: 'nav',
+		easing: 'easeInOutExpo',
+		opacity: [0, 1],
+		translateY: [-20, 0],
+		duration: 1200,
+	})
+}
+
 if (window.innerWidth > 600) {
 	scroll = new LocomotiveScroll({
 		el: document.querySelector('[data-scroll-container]'),
 		smooth: true,
 		repeat: true
 	})
+
+	splitting()
+	introAnim()
 }
 
 
@@ -26,22 +71,6 @@ if (scroll) {
 			scroll.scrollTo(document.querySelector(linkEl.dataset.link))
 		})
 	})
-
 }
-
-// splitting()
-
-// anime({
-// 	targets: '.header-dates > span > .char',
-// 	translateY: -10,
-// 	delay: anime.stagger(100)
-// })
-
-// anime({
-// 	targets: 'nav',
-// 	opacity: 1,
-// 	duration: 1000,
-// 	delay: 5000
-// })
 
 starryNight()
